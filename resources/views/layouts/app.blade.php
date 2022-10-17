88a7694e8e3ba7527aa55046f963b1904feb79e2
Wisp X <1591788658@qq.com>
2022-10-17 14:02:23 +0800

    fix a bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title', 'Photograph')</title>
+        <title>@yield('title', config('app.name'))</title>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
